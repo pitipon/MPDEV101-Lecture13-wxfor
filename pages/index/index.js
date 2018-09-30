64b1a4acaea211cb7461230441efc09020ebb2a0
Pitipon Pluemworasawat <pitipon@gmail.com>
2018-10-01 00:49:02 +0700

    done

---
+++ pages/index/index.js
@@ -0,0 +1,18 @@
+//index.js
+const app = getApp()
+
+Page({
+  data: {
+    chats : [ 
+      { name: "Pop", message: "Yeah .. sure" },
+      { name: "mo", message: "I want to be friend with u" },
+      { name: "Pop", message: "I am pop" },
+      { name: "Pop", message: "Nice to meet you" },
+      { name: "mo", message: "Heyyyy, I am MO" },
+    ] 
+  },
+  
+  onLoad: function () {
+    
+  }
+})
